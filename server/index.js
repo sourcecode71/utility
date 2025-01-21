77787703482bd39f62ee4f70fc2be93d29554bf8
Mostafiz <mostafiz571@gmail.com>
2025-01-21 19:02:27 +0600

    add docker and compose files

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ const winston = require('winston');
 const express = require('express');
 const app = new express();
 
+console.log('Starting the server...');
 require('./startup/logging')();
+console.log('Logging initialized...');
 require('./startup/routes')(app);
+console.log('Routes initialized...');
 require('./startup/db')();
-//require('./startup/config')();
+console.log('Database initialized...');
+// require('./startup/config')();
+// console.log('Config initialized...');
 require('./startup/validation')();
+console.log('Validation initialized...');
+
 
 const port = process.env.PORT || 3001;
 const server = app.listen(port, ()=> winston.info(`Listening on port ${port}...`));
